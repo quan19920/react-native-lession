6804121fddbbac65e2278c2002444434549b833c
quan19920 <69345183+quan19920@users.noreply.github.com>
2020-08-11 10:53:44 +0700

    login apptuha

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import Constants from 'expo-constants'
+import Login from './components/login'
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View><Login /></View>
     </View>
   );
 }
@@ -14,8 +15,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    //marginTop: Constants.statusBarHeight,
   },
+  
 });
--- components/login.js
+++ components/login.js
@@ -1,12 +1,104 @@
 import React from 'react'
-import { View } from 'react-native'
+import { View, StyleSheet, ImageBackground, Text, Button, Alert } from 'react-native'
+import { LinearGradient } from 'expo-linear-gradient'
+
 
 function login() {
     return (
-        <View>
-            
-        </div>
+        <View style={styles.container}>
+            <LinearGradient 
+            colors={['#2974FA', '#38ABFD', '#43D4FF']} 
+            style={{
+                position: 'absolute',
+                left: 0,
+                right: 0,
+                top: 0,
+                height:1000,
+              }}
+           >        
+            <View style={styles.up}><ImageBackground source={require('./anh/tuha_logo.png')} style={styles.logo} /></View>
+            <View style={styles.down}>
+                <View style={styles.down1}>
+                    <View style={styles.login}>
+                        <View style={styles.textlogin}>
+                            <Text>set up khung đăng kí </Text>
+                            <Text>set up khung đăng kí </Text>
+                        </View>
+                        <View style={styles.buttonlogin}>
+                            <Button title="LOGIN" onPress={()=>{Alert.alert("Login successed")}} style={styles.button} ></Button>
+                        </View>
+                        
+                    </View>
+                </View>
+                <View style={styles.down2}></View>
+            </View>
+
+        </LinearGradient>
+        </View>
     )
 }
 
 export default login
+
+const styles = StyleSheet.create({
+    container: {
+        flex:1,
+        //marginTop: Constants.statusBarHeight,
+    },
+
+    up:{
+        flex:1,
+        justifyContent:"flex-end",
+        paddingBottom:20,
+        flexDirection: 'column',
+        alignItems: 'center',
+    },
+    
+    logo :{
+        width:120,
+        height:120,  
+    },
+    
+    down:{
+        flex:3, 
+        alignItems: 'center',
+        justifyContent: 'center',
+        
+    },
+    down1:{
+        flex:1,
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        width:320,
+        backgroundColor: "rgba(255,255,255,0.5)",
+        borderTopLeftRadius: 40,
+        borderBottomRightRadius: 40,
+        borderTopRightRadius: 40,
+        borderBottomLeftRadius: 40,
+
+    },
+    down2:{
+        flex:2,
+    },
+    login:{
+        flex:1,
+        backgroundColor:'#dbd6d3',
+        resizeMode: "cover",
+        width: 250,
+        height: 200,
+        borderColor: "#d311de",
+        position: 'absolute',
+        alignItems:'center'
+    },
+    textlogin:{
+        flex:3,
+    },
+    buttonlogin:{
+        flex:1,
+    },
+    button:{
+        
+        
+    },
+});
